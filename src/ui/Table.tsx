f666f22138e723ba1aecfaa6389a365974b0eb3d
tulkin07 <akhmadov0770@gmail.com>
2024-04-18 21:17:35 +0500

    add in table icons

--- src/ui/Table.tsx
+++ src/ui/Table.tsx
@@ -4,11 +4,10 @@ function Table() {
       <table className="table">
         <thead>
           <tr>
-            <th><input type="checkbox" /></th>
+            <th className="table__checkbox"><input type="checkbox" /></th>
             <th>Id</th>
             <th>Received</th>
-            <th>Note</th>
-            <th>User</th>
+            <th className="table__user">User</th>
             <th>Customer</th>
             <th>Phone</th>
             <th>Vehicle</th>
@@ -22,11 +21,24 @@ function Table() {
             <td><input type="checkbox" /></td>
             <td><span className="table__id">600009</span></td>
             <td>03/25/2024 03:00 PM</td>
-            <td>0</td>
-            <td><img src="./img/dt_table/default_user_image.png" alt="" className="table__user__img" /></td>
+            <td>
+              <div className="table__img__container">
+                <img src="./img/dt_table/default_user_image.png" alt="" className="table__user__img" />
+              </div>
+            </td>
             <td>Ali Brian</td>
             <td>22222222222</td>
-            <td>...</td>
+            <td>
+              <div className="table__vehicle">
+                <div className="table__vehicle__imgs">
+                  <img src="./img/dt_table/engine.svg" alt="engine" />
+                  <img src="./img/dt_table/trailer.svg" alt="trailer" />
+                </div>
+                <div className="table__vehicle__text">
+                  2022 Toyota Sienna
+                </div>
+              </div>
+            </td>
             <td>California, CA 91101</td>
             <td>Maine, ME 04735</td>
             <td>2024-03-26</td>
@@ -35,12 +47,24 @@ function Table() {
             <td><input type="checkbox" /></td>
             <td><span className="table__id">600009</span></td>
             <td>03/25/2024 03:00 PM</td>
-            <td>0</td>
-
-            <td><img src="./img/dt_table/default_user_image.png" alt="" className="table__user__img" /></td>
+            <td>
+              <div className="table__img__container">
+                <img src="./img/dt_table/default_user_image.png" alt="" className="table__user__img" />
+              </div>
+            </td>
             <td>Ali Brian</td>
             <td>22222222222</td>
-            <td>...</td>
+            <td>
+              <div className="table__vehicle">
+                <div className="table__vehicle__imgs">
+                  <img src="./img/dt_table/engine.svg" alt="engine" />
+                  <img src="./img/dt_table/trailer.svg" alt="trailer" />
+                </div>
+                <div className="table__vehicle__text">
+                  2022 Toyota Sienna
+                </div>
+              </div>
+            </td>
             <td>California, CA 91101</td>
             <td>Maine, ME 04735</td>
             <td>2024-03-26</td>
@@ -49,12 +73,24 @@ function Table() {
             <td><input type="checkbox" /></td>
             <td><span className="table__id">600009</span></td>
             <td>03/25/2024 03:00 PM</td>
-            <td>0</td>
-
-            <td><img src="./img/dt_table/default_user_image.png" alt="" className="table__user__img" /></td>
+            <td>
+              <div className="table__img__container">
+                <img src="./img/dt_table/default_user_image.png" alt="" className="table__user__img" />
+              </div>
+            </td>
             <td>Ali Brian</td>
             <td>22222222222</td>
-            <td>...</td>
+            <td>
+              <div className="table__vehicle">
+                <div className="table__vehicle__imgs">
+                  <img src="./img/dt_table/engine.svg" alt="engine" />
+                  <img src="./img/dt_table/trailer.svg" alt="trailer" />
+                </div>
+                <div className="table__vehicle__text">
+                  2022 Toyota Sienna
+                </div>
+              </div>
+            </td>
             <td>California, CA 91101</td>
             <td>Maine, ME 04735</td>
             <td>2024-03-26</td>
@@ -63,12 +99,50 @@ function Table() {
             <td><input type="checkbox" /></td>
             <td><span className="table__id">600009</span></td>
             <td>03/25/2024 03:00 PM</td>
-            <td>0</td>
-
-            <td><img src="./img/dt_table/default_user_image.png" alt="" className="table__user__img" /></td>
+            <td>
+              <div className="table__img__container">
+                <img src="./img/dt_table/default_user_image.png" alt="" className="table__user__img" />
+              </div>
+            </td>
             <td>Ali Brian</td>
             <td>22222222222</td>
-            <td>...</td>
+            <td>
+              <div className="table__vehicle">
+                <div className="table__vehicle__imgs">
+                  <img src="./img/dt_table/engine.svg" alt="engine" />
+                  <img src="./img/dt_table/trailer.svg" alt="trailer" />
+                </div>
+                <div className="table__vehicle__text">
+                  2022 Toyota Sienna
+                </div>
+              </div>
+            </td>
+            <td>California, CA 91101</td>
+            <td>Maine, ME 04735</td>
+            <td>2024-03-26</td>
+          </tr>
+          <tr>
+            <td><input type="checkbox" /></td>
+            <td><span className="table__id">600009</span></td>
+            <td>03/25/2024 03:00 PM</td>
+            <td>
+              <div className="table__img__container">
+                <img src="./img/dt_table/default_user_image.png" alt="" className="table__user__img" />
+              </div>
+            </td>
+            <td>Ali Brian</td>
+            <td>22222222222</td>
+            <td>
+              <div className="table__vehicle">
+                <div className="table__vehicle__imgs">
+                  <img src="./img/dt_table/engine.svg" alt="engine" />
+                  <img src="./img/dt_table/trailer.svg" alt="trailer" />
+                </div>
+                <div className="table__vehicle__text">
+                  2022 Toyota Sienna
+                </div>
+              </div>
+            </td>
             <td>California, CA 91101</td>
             <td>Maine, ME 04735</td>
             <td>2024-03-26</td>
