504d8d4bdc952854f5dc553af4c6904b9916da70
j.bektemirov <b.o.jakhongir@gmail.com>
2024-04-19 16:58:04 +0500

    change leads

--- src/ui/PageDetails.tsx
+++ src/ui/PageDetails.tsx
@@ -1,58 +1,62 @@
-import { useState } from "react"
-
+import { useState } from 'react';
 
 export default function PageDetails() {
-    const [isActive, ] = useState(false);
-    return (
-        <div className="details">
-            <div className="details__header">
-                <div className="details__header__col">
-                    {
-                        isActive ?
-                            <div className="details__header__controller-left">
-                                <span><img src="./img/details/close-right.svg" alt="" /></span>
-                                <span><img src="./img/details/resize.svg" alt="" /></span>
-                                <span><img src="./img/details/up-arrow.svg" alt="" /></span>
-                                <span><img src="./img/details/down-arrow.svg" alt="" /></span>
-                            </div>
-                            :
-                            <div className="details__header__controller">
-                                <span><img src="./img/details/close.svg" alt="" /></span>
-                                <span><img src="./img/details/up-arrow.svg" alt="" /></span>
-                                <span><img src="./img/details/down-arrow.svg" alt="" /></span>
-                                <span><img src="./img/details/resize.svg" alt="" /></span>
-                            </div>
-                    }
-
-
-
-                    <div className="details__header__userid">
-                        #600009
-                    </div>
-                    <div className="details__header__username">
-                        Ali Brian
-                    </div>
-                </div>
-                <div className="details__header__col">
-                    <div className="details__header__user">
-                        <img src="./img/main_user_img.svg" alt="" />
-                        <div className="details__header__select">
-                            <select >
-                                <option >Ali Brian</option>
-                            </select>
-                        </div>
-                    </div>
-                    <button className="details__header__archive">Archive</button>
-                    <span className="details__header__more">
-                        <img src="./img/dt_table/more.svg" alt="" />
-                    </span>
-                </div>
+  const [isActive] = useState(true);
+  return (
+    <div className="details">
+      <div className="details__header">
+        <div className="details__header__col">
+          {isActive ? (
+            <div className="details__header__controller-left">
+              <span>
+                <img src="./img/details/close-right.svg" alt="" />
+              </span>
+              <span>
+                <img src="./img/details/resize.svg" alt="" />
+              </span>
+              <span>
+                <img src="./img/details/up-arrow.svg" alt="" />
+              </span>
+              <span>
+                <img src="./img/details/down-arrow.svg" alt="" />
+              </span>
             </div>
-            <div className="details__content">
-                ...
+          ) : (
+            <div className="details__header__controller">
+              <span>
+                <img src="./img/details/close.svg" alt="" />
+              </span>
+              <span>
+                <img src="./img/details/up-arrow.svg" alt="" />
+              </span>
+              <span>
+                <img src="./img/details/down-arrow.svg" alt="" />
+              </span>
+              <span>
+                <img src="./img/details/resize.svg" alt="" />
+              </span>
             </div>
+          )}
+
+          <div className="details__header__userid">#600009</div>
+          <div className="details__header__username">Ali Brian</div>
         </div>
-    )
+        <div className="details__header__col">
+          <div className="details__header__user">
+            <img src="./img/main_user_img.svg" alt="" />
+            <div className="details__header__select">
+              <select>
+                <option>Ali Brian</option>
+              </select>
+            </div>
+          </div>
+          <button className="details__header__archive">Archive</button>
+          <span className="details__header__more">
+            <img src="./img/dt_table/more.svg" alt="" />
+          </span>
+        </div>
+      </div>
+      <div className="details__content">...</div>
+    </div>
+  );
 }
-
-
--- src/ui/Table.tsx
+++ src/ui/Table.tsx
@@ -1,18 +1,19 @@
 function Table() {
-
   const onChecked = () => {
-    const d = document.querySelectorAll("input");
+    const d = document.querySelectorAll('input');
     console.log(d);
 
-    d.forEach(item => item.checked)
-  }
+    d.forEach((item) => item.checked);
+  };
 
   return (
     <div className="table__container">
       <table className="table">
         <thead>
           <tr>
-            <th className="table__checkbox"><input type="checkbox" onChange={onChecked} /></th>
+            <th className="table__checkbox">
+              <input type="checkbox" onChange={onChecked} />
+            </th>
             <th>Id</th>
             <th>Received</th>
             <th className="table__user">User</th>
@@ -27,16 +28,23 @@ function Table() {
         </thead>
         <tbody>
           <tr>
-            <td><input type="checkbox" /></td>
-            <td><span className="table__id">600009</span></td>
+            <td>
+              <input type="checkbox" />
+            </td>
+            <td>
+              <span className="table__id">600009</span>
+            </td>
             <td>03/25/2024 03:00 PM</td>
             <td>
               <div className="table__img__container">
-                <img src="./img/dt_table/default_user_image.png" alt="" className="table__user__img" />
+                <img
+                  src="./img/dt_table/default_user_image.png"
+                  alt=""
+                  className="table__user__img"
+                />
               </div>
             </td>
             <td>
-
               <div className="table__node">
                 <div className="table__node__data">
                   <img src="./img/dt_table/nodeimg.png" alt="" />
@@ -44,7 +52,9 @@ function Table() {
                     <span>25</span>
                     <div className="table__node__modal">
                       <div className="table__node__content">
-                        <div className="node__date">01/21/2024 09:01 AM Ali Brian</div>
+                        <div className="node__date">
+                          01/21/2024 09:01 AM Ali Brian
+                        </div>
                         <p className="node__description">
                           Customer wants to talk to customer
                         </p>
@@ -54,25 +64,20 @@ function Table() {
                 </div>
               </div>
             </td>
-            <td>
-              Ali Brian
-
-            </td>
-
+            <td>Ali Brian</td>
             <td>
               <div className="table__phone">
                 <img src="./img/dt_table/call.svg" alt="" />
                 <span>(999) 999-9999</span>
               </div>
-            </td>            <td>
+            </td>{' '}
+            <td>
               <div className="table__vehicle">
                 <div className="table__vehicle__imgs">
                   <img src="./img/dt_table/engine.svg" alt="engine" />
                   <img src="./img/dt_table/trailer.svg" alt="trailer" />
                 </div>
-                <div className="table__vehicle__text">
-                  2022 Toyota Sienna
-                </div>
+                <div className="table__vehicle__text">2022 Toyota Sienna</div>
               </div>
             </td>
             <td>California, CA 91101</td>
@@ -82,7 +87,6 @@ function Table() {
         </tbody>
       </table>
     </div>
-
   );
 }
 
