1e021db3411b2bb1b95e6b12d3d4de636b04eb35
tulkin07 <akhmadov0770@gmail.com>
2024-04-23 15:46:53 +0500

    remove commit

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,7 +1,5 @@
 import {  Outlet, } from "react-router-dom";
 import SettingSidebar from "../ui/SettingSidebar";
-// import Users from "./settingPages/Users";
-// import Users from "./settingPages/Users";
 
 function Settings() {
   return (
